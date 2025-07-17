c87bc65b2482a1b8f3010033f0bfd6c7157abe24
Nirmal <nirmalsep2001@gmail.com>
2025-07-17 10:39:15 +0530

    Backend URL changed

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 // CORS Configuration
 const corsOptions = {
   origin: [
-    'https://hrms-backend-production-abd6.up.railway.app',
+    'https://hrms-backend-5wau.onrender.com/',
     'http://localhost:3000',
     'https://portal.thevsoft.com',
     'http://localhost:5173',
